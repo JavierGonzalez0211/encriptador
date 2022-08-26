68cd381bf682f3918dd7a46da97ac0e18320327d
JavierGonzalez0211 <ejgcontador.uba@gmail.com>
2022-08-26 17:54:27 -0300

    clean the message from accents and special characters, add Readme.md

---
+++ README.md
@@ -0,0 +1,27 @@
+# Curso Full Stack ORACLE NEXT EDUCATION - ALURA
+
+## Primer Desafío
+
+### <strong>El desafío es crear una aplicación que utilizando claves sencillas se puedan encriptar y desencriptar textos</strong>
+
+<br>
+
+## Requisitos:
+
+- Debe funcionar solo con letras minúsculas
+- No deben ser utilizados letras con acentos
+- Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+
+Por ejemplo:
+"gato" => "gaitober"
+gaitober" => "gato"
+
+La página debe tener campos para
+inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre las dos opciones.
+El resultado debe ser mostrado en la pantalla.
+<br>
+<br>
+
+### Extras:
+
+- Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ let encryptedMessage = document.querySelector(".encrypted-message");
 const noResultArea = document.querySelector(".no-message-encrypted");
 const resultArea = document.querySelector(".result-display");
 
+function stringNormalization(message) {
+  let r = message.toLowerCase();
+  r = r.replace(new RegExp(/[àáâãäå]/g), "a");
+  r = r.replace(new RegExp(/æ/g), "ae");
+  r = r.replace(new RegExp(/ç/g), "c");
+  r = r.replace(new RegExp(/[èéêë]/g), "e");
+  r = r.replace(new RegExp(/[ìíîï]/g), "i");
+  r = r.replace(new RegExp(/[òóôõö]/g), "o");
+  r = r.replace(new RegExp(/œ/g), "oe");
+  r = r.replace(new RegExp(/[ùúûü]/g), "u");
+  r = r.replace(new RegExp(/[ýÿ]/g), "y");
+  return r;
+}
+
 function encrypt(message) {
   let keyValueEncryptor = [
     ["e", "enter"],
@@ -11,7 +25,7 @@ function encrypt(message) {
     ["o", "ober"],
     ["u", "ufat"],
   ];
-  message = message.toLowerCase();
+  message = stringNormalization(message);
 
   for (let i = 0; i < keyValueEncryptor.length; i++) {
     if (message.includes(keyValueEncryptor[i][0])) {
@@ -32,7 +46,7 @@ function decrypt(message) {
     ["o", "ober"],
     ["u", "ufat"],
   ];
-  message = message.toLowerCase();
+  message = stringNormalization(message);
 
   for (let i = 0; i < keyValueEncryptor.length; i++) {
     if (message.includes(keyValueEncryptor[i][1])) {
